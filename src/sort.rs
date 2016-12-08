d48a86a423cfa4013d50fecf0391d364fe419e5e
Lee Bousfield <ljbousfield@gmail.com>
2016-12-08 10:49:06 -0700

    Sort: Joiner -> Option<Spawner>

--- src/sort.rs
+++ src/sort.rs
@@ -1,95 +1,40 @@
 use std::cmp::Ordering;
-
 use std::fmt::Debug;
 
 use jobsteal::Pool;
 use jobsteal::Spawner;
 
-trait Joiner {
-    type NewJoiner: Joiner;
-
-    fn is_parallel() -> bool;
-    fn join<A, RA, B, RB>(&self, oper_a: A, oper_b: B) -> (RA, RB)
-        where A: FnOnce(Self::NewJoiner) -> RA + Send,
-              B: FnOnce(Self::NewJoiner) -> RB + Send,
-              RA: Send,
-              RB: Send,
-              Self: Sized;
-}
-
-struct Parallel<'s, 'p: 's> {
-    spawner: &'s Spawner<'p, 's>,
-}
-
-impl<'s, 'p, 'n> Joiner for Parallel<'s, 'p> {
-    type NewJoiner = Parallel<'s, 'n>;
-
-    #[inline]
-    fn is_parallel() -> bool {
-        true
-    }
-    #[inline]
-    fn join<A, RA, B, RB>(&self, oper_a: A, oper_b: B) -> (RA, RB)
-        where A: FnOnce(Parallel<'s, 'n>) -> RA + Send,
-              B: FnOnce(Parallel<'s, 'n>) -> RB + Send,
-              RA: Send,
-              RB: Send
-    {
-        self.spawner.join(|s| {
-                              let joiner = Parallel { spawner: s };
-                              oper_a(joiner)
-                          },
-                          |s| {
-                              let joiner = Parallel { spawner: s };
-                              oper_b(joiner)
-                          })
-    }
-}
-
-struct Sequential;
-impl Joiner for Sequential {
-    type NewJoiner = Sequential;
-
-    #[inline]
-    fn is_parallel() -> bool {
-        false
-    }
-    #[inline]
-    fn join<A, RA, B, RB>(&self, oper_a: A, oper_b: B) -> (RA, RB)
-        where A: FnOnce(Self) -> RA,
-              B: FnOnce(Self) -> RB
-    {
-        let a = oper_a(Sequential {});
-        let b = oper_b(Sequential {});
-        (a, b)
-    }
-}
-
-fn quick_sort<J: Joiner,
-              K: Ord + Clone + Send + Debug,
+fn quick_sort<K: Ord + Clone + Send + Debug,
               V: Send + Debug,
               F: Fn(&K, &mut V, V) + Sync>
     (v: &mut [Option<(K, V)>],
      merge_dups: &F,
-     joiner: J) {
+     spawner: Option<&Spawner>) {
     if v.len() <= 1 {
         return;
     }
 
-    if J::is_parallel() && v.len() <= 5 * 1024 {
-        return quick_sort(v, merge_dups, Sequential {});
+    if spawner.is_some() && v.len() <= 5 * 1024 {
+        return quick_sort(v, merge_dups, None);
     }
 
     let split_point = partition(v, merge_dups);
     let (lo, hi) = v.split_at_mut(split_point);
-    joiner.join(|j| quick_sort(lo, merge_dups, j),
-                |j| quick_sort(hi, merge_dups, j));
+
+    if let Some(spawner) = spawner {
+        spawner.join(|j| quick_sort(lo, merge_dups, Some(j)),
+                     |j| quick_sort(hi, merge_dups, Some(j)));
+    } else {
+        quick_sort(lo, merge_dups, None);
+        quick_sort(hi, merge_dups, None);
+    }
 }
 
 pub fn sort<K: Ord + Clone + Send + Debug,
             V: Send + Debug,
             F: Fn(&K, &mut V, V) + Sync>(v: &mut [Option<(K, V)>], merge_dups: F, pool: Pool) {
-    quick_sort(v, &merge_dups, Parallel { spawner: &pool.spawner() })
+    let spawner = pool.spawner();
+    quick_sort(v, &merge_dups, Some(&spawner))
 }
 
 fn compare<K: Ord, V>(a: &Option<(K, V)>, b: &Option<(K, V)>) -> Ordering {
