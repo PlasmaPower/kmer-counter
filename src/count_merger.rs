e53909f223c0dc2b160250d2539a0b55f4f20ef9
Lee Bousfield <ljbousfield@gmail.com>
2016-12-16 10:44:04 -0700

    Begin Collector abstraction

--- src/count_merger.rs
+++ src/count_merger.rs
@@ -1,3 +1,5 @@
+use kmer_length::KmerLength;
+
 #[derive(PartialEq, Eq, Clone)]
 pub enum JoinMethod {
     Concat,
@@ -5,9 +7,45 @@ pub enum JoinMethod {
     Sort,
 }
 
-// TODO: Figure out how this works with Mutex
 pub trait Collector {
-    fn add(&mut self, count: (u64, u16));
-    fn finalize(&mut self);
+    fn push(&mut self, count: (u64, u16));
+    fn finalize(&mut self) {}
     fn subdivide(&mut self, join_method: JoinMethod) -> Box<&mut Collector>;
+}
+
+pub struct OutputCollector<T> {
+    out: BufWriter<T>,
+    kmer_len: KmerLength,
+    min_len: u16,
+}
+
+impl<T: Write> OutputCollector<T> {
+    pub fn new(out: T, kmer_len: KmerLength, min_len: u16) {
+        OutputCollector {
+            out: BufWriter::new(out),
+            kmer_len: kmer_len,
+            min_len: min_len,
+        }
+    }
+}
+
+impl<T: Write> Collector for OutputCollector<T> {
+    fn push(&mut self, (kmer, count): (u64, u16)) {
+        if count < self.min_count {
+            continue;
+        }
+        let mut kmer_str = vec![0; kmer_len + 1];
+        kmer_str[kmer_len] = b'\t';
+        for i in (0..kmer_len).rev() {
+            let nucleotide = Nucleotide::from_lower_bits(kmer as u8);
+            let chr = nucleotide.as_text_byte();
+            kmer_str[i] = chr;
+            kmer = kmer >> 2;
+        }
+        stream.write(kmer_str.as_slice())
+            .and_then(|_| stream.write(count.to_string().as_bytes()))
+            .and_then(|_| stream.write(b"\n"))
+            .chain_err(|| "Failed to write k-mer to output stream")
+            .unwrap();
+    }
 }
